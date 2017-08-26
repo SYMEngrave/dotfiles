05b79edae2fd86bbebe6cfa0faa4f840e8cb093c
sym <sym@symdeMacBook-Pro.local>
2017-08-26 18:19:04 +0800

    add zshrc

---
+++ zsh/install-oh-my-zsh.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#########################################################################
+# File Name: install-oh-my-zsh.sh
+# Author: Yiming Sun
+# mail: symengrave@gmail.com
+# Created Time: 六  8/26 18:11:45 2017
+#########################################################################
+
+sh -C "$(curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh)"
